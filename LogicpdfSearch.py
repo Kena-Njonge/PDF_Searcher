5270046854204e6c25f3fa8622022d9672113cca
Kena-Njonge <75584199+Kena-Njonge@users.noreply.github.com>
2022-10-04 23:05:21 +0200

    v1.1
    Update to supported quotation.
    
    Now supports both quoted and unquoted arguments e.g.
    "Cat dog mouse" or Cat dog mouse
    "F:\Users\Downloads\Target" or 'F:\Users\Downloads\Target' or F:\Users\Downloads\Target
    "Filename" or Filename

--- LogicpdfSearch.py
+++ LogicpdfSearch.py
@@ -3,13 +3,13 @@ import PyPDF2
 import re
 import os.path
 
-def textSearch(targetpath,wordlist,name,returnpath):
-    x = len(targetpath)
-    #differentiation between path with or without quotation marks
-    if targetpath[0] == '"' or targetpath[0]== "'" : 
-        object = PyPDF2.PdfFileReader(r"{}".format(targetpath[1:x-1]))
+def textSearch(target_path,word_list,name,return_path):
+    x = len(target_path)
+    #differentiation between Target path with or without quotation marks
+    if target_path[0] == '"' or target_path[0]== "'" : 
+        object = PyPDF2.PdfFileReader(r"{}".format(target_path[1:x-1]))
     else :
-        object = PyPDF2.PdfFileReader(r"{}".format(targetpath))
+        object = PyPDF2.PdfFileReader(r"{}".format(target_path))
     #create new PDF
     writer = PyPDF2.PdfWriter()
     # get number of pages
@@ -18,20 +18,26 @@ def textSearch(targetpath,wordlist,name,returnpath):
     i = 0
     #Page visit marker
     VisitedPage = [0] * NumPages
-
-    
     for i in range(0, NumPages):
         #Extract text
         PageObj = object.getPage(i)
         Text = PageObj.extractText() 
         #Search page for target words
-        for x in wordlist:
-            ResSearch = re.search(x, Text)
+        for x in word_list:
+            #Two regular expression searches for possible types of quoations
+            ResSearch = re.search(x.replace("'",""), Text)
+            ResSearch2 = re.search(x.replace('"',''), Text)
+            print(x)
             #Add pages with target text and avoid duplicate additions
-            if ResSearch != None and VisitedPage[i]==0:
+            if (ResSearch != None or ResSearch2 != None)  and VisitedPage[i]==0:
                 writer.addPage(object.getPage(i))
                 VisitedPage[i] = 1
-
-    #Save pdf file under name chosen by user
-    with open(os.path.join(returnpath,name + ".pdf"), "wb") as f:
-        writer.write(f)
+    #Remove quotation marks from name
+    name = name.replace("'","")
+    name = name.replace('"','')
+    #differentiation between Return path with or without quotation marks
+    if return_path[0] == '"' or return_path[0]== "'" : 
+        return_path= return_path.replace("'",'')
+        return_path= return_path.replace('"','')
+    with open(os.path.join(return_path,name + ".pdf"), "wb") as f:
+            writer.write(f)
